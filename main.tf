24aa267b277c4e32ba33f65c9b9540a046b55556
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-07-15 11:35:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = ap-south-1
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_first_ec2" {
-  ami                    = ami-0f918f7e67a3323f0
-  instance_type          = t2.micro
+  ami                    = "ami-0f918f7e67a3323f0"
+  instance_type          = "t2.micro"
   tags = {
     Name = "JN-TF-EC2"
   }
